f3a3541d94a6148dd4d26e52c08d8490da9cfebe
MicheleDelliVeneri <micheledelliveneri@gmail.com>
2023-02-16 14:56:45 +0100

    added new plotting in tester

--- utils/tester.py
+++ utils/tester.py
@@ -169,8 +169,31 @@ def get_TNG_dataloaders(data_path, catalogue_path, bands, targets, train_size, t
     test_dataloader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers, pin_memory=True)
     return training_dataloader, validation_dataloader, test_dataloader
 
+def plotter(train_loader, batch_size, output_path, mode):
+    for i_batch, batch in enumerate(train_loader, total=len(train_loader), desc='Plotting Samples from {} set'.format(mode)):
+        inputs = torch.permute(batch['input'][tio.DATA], (0, 4, 2, 3, 1)).numpy()
+        targets = torch.permute(batch['target'][tio.DATA], (0, 4, 2, 3, 1)).numpy()
+        ids = batch['id']
+        orientations = batch['orientation']
+        for i in range(batch_size):
+            input_ = inputs[i][0]
+            target = targets[i][0]
+            fig, axs = plt.subplots(nrows=2, ncols=len(bands) // 2, figsize=(5 * len(bands) // 2, 10))
+            for k in range(2):
+                for j in range(len(bands)  // 2):
+                    im = axs[k, j].imshow(np.log(input_[:, :, j] + np.min(input_[:, :, j]) + 1e-10), origin='lower', cmap='magma')
+                    axs[k, j].set_title('{}'.format(bands[j]))
+                    axs[k, j].set_xlabel('x')
+                    axs[k, j].set_ylabel('y')
+                    plt.colorbar(im, ax=axs[k, j])
+        plt.tight_layout()
+        plt.savefig(os.path.join(output_path, 'Input_Bands_{}_{}_{}.png'.format(i, ids[i], orientations[i])))
+        plt.close()
+
+       
+
 catalogue_path = '/ibiscostorage/mdelliveneri/EUCLID/version2/catalog/galaxyCatalogue.txt'
-path = '/ibiscostorage/mdelliveneri/EUCLID/version2/results'
+data_path = '/ibiscostorage/mdelliveneri/EUCLID/version2/results'
 all_bands = ['MASS_H', '2MASS_J', '2MASS_Ks', 'Euclid_H', 'Euclid_J', 'Euclid_VIS', 'Euclid_Y', 'GALEX_FUV', 'GALEX_NUV', 'Johnson_B', 'Johnson_I', 
         'Johnson_R', 'Johnson_U', 'Johnson_V', 'LSST_g', 'LSST_i', 'LSST_r', 'LSST_u', 'LSST_y', 'LSST_z', 'WISE_W1', 'WISE_W2']
 
@@ -181,13 +204,104 @@ if os.path.exists(output_path) == False:
     os.mkdir(output_path)
 
 target_name = 'dustmass'
-train_dataloader, validation_dataloader, test_dataloader = get_TNG_dataloaders(path, catalogue_path, bands, target_name, train_size=0.8, batch_size=batch_size)
-batch = next(iter(train_dataloader))
-inputs = torch.permute(batch['input'][tio.DATA], (0, 4, 2, 3, 1)).numpy()
-targets = torch.permute(batch['target'][tio.DATA], (0, 4, 2, 3, 1)).numpy()
-ids = batch['id']
-orientations = batch['orientation']
+#train_dataloader, validation_dataloader, test_dataloader = get_TNG_dataloaders(path, catalogue_path, bands, target_name, train_size=0.8, batch_size=batch_size)
+#batch = next(iter(train_dataloader))
+#inputs = torch.permute(batch['input'][tio.DATA], (0, 4, 2, 3, 1)).numpy()
+#targets = torch.permute(batch['target'][tio.DATA], (0, 4, 2, 3, 1)).numpy()
+#ids = batch['id']
+#orientations = batch['orientation']
 
+catalogue = dl.load_catalogue(catalogue_path)
+idlist = catalogue['subhalo ID'].values
+euclid_lsst_dir = os.path.join(data_path, 'euclid+lsst')
+galex_dir = os.path.join(data_path, 'galex')
+wise_dir = os.path.join(data_path, 'wise')
+other_dir = os.path.join(data_path, 'otherbands')
+targets_dir = os.path.join(data_path, 'targets')
+euclid_files = [file for file in os.listdir(euclid_lsst_dir) if 'Euclid' in file]
+lsst_files = [file for file in os.listdir(euclid_lsst_dir) if 'LSST' in file]
+galex_files = [file for file in os.listdir(galex_dir) if 'GALEX' in file]
+wise_files = [file for file in os.listdir(wise_dir) if 'WISE' in file]
+two_mass_files = [file for file in os.listdir(other_dir) if '2MASS' in file]
+johnson_files = [file for file in os.listdir(other_dir) if 'Johnson' in file]
+band_info = {}
+for band in bands:
+    if 'Euclid' in band:
+        band_files = [os.path.join(euclid_lsst_dir, file) for file in euclid_files if band in file]
+    if 'LSST' in band:
+        band_files = [os.path.join(euclid_lsst_dir, file) for file in lsst_files if band in file]
+    if 'GALEX' in band:
+        band_files = [os.path.join(galex_dir, file) for file in galex_files if band in file]
+    if 'WISE' in band:
+        band_files = [os.path.join(wise_dir, file) for file in wise_files if band in file]
+    if 'MASS' in band:
+        band_files = [os.path.join(other_dir, file) for file in two_mass_files if band in file]
+    if 'Johnson' in band:
+        band_files = [os.path.join(other_dir, file) for file in johnson_files if band in file]
+       
+    band_info[band] = {}
+    band_info[band]['files'] = band_files
+    band_info[band]['ids'] = np.array([int("".join([t for t in tid.split('_')[0].split('/')[-1] if t.isdigit()])) for tid in band_files])
+    band_info[band]['orientations'] = np.array([int(tid.split('_')[1][1]) for tid in band_files])
+    band_info[band]['len'] = len(band_files) // len(np.unique(band_info[band]['orientations']))
+    
+target_info = {}
+targets_files = [os.path.join(targets_dir, file) for file in os.listdir(targets_dir) if target_name in file]
+target_info['targets'] = np.array(targets_files)
+target_info['ids'] = np.array([int("".join([t for t in tid.split('_')[0].split('/')[-1] if t.isdigit()])) for tid in targets_files])
+target_info['orientations'] = np.array([int(tid.split('_')[1][1]) for tid in targets_files])
+target_info['len'] = len(targets_files) // len(np.unique(target_info['orientations']))
+    
+print('You have selected the following bands:')
+for key, value in band_info.items():
+    print(key, value['len'], len(np.unique(value['orientations'])), len(value['ids']))
+print('\n')
+print('and the following target:')
+print(target_name, target_info['len'], len(np.unique(target_info['orientations'])), len(target_info['ids']))
+print('Number of simulations in catalogue: ', len(idlist))
+catalogue = catalogue[catalogue['subhalo ID'].isin(target_info['ids'])]
+idlist = catalogue['subhalo ID'].values
+print('Number of simulations in catalogue after filtering based on targets: ', len(idlist))
+print('Removing simualtions with no target counterpart:')
+for key, value in band_info.items():
+    temp_files = band_info[key]['files']
+    temp_ids = band_info[key]['ids']
+    temp_orientations = band_info[key]['orientations']
+    band_info[key]['files'] = np.array([temp_files[i] for i in range(len(temp_files)) if band_info[key]['ids'][i] in idlist])
+    band_info[key]['orientations'] = np.array([temp_orientations[i] for i in range(len(temp_orientations)) if band_info[key]['ids'][i] in idlist])
+    band_info[key]['ids'] = np.array([temp_ids[i] for i in range(len(temp_ids)) if band_info[key]['ids'][i] in idlist])
+    band_info[key]['len'] = len(band_info[key]['files']) // len(np.unique(band_info[key]['orientations']))
+    
+for key, value in band_info.items():
+    print(key, value['len'], len(np.unique(value['orientations'])), len(value['ids']))
+print('\n')
+
+inputs_catalogue = {}
+targets_catalogue = {}
+n_orientations = len(np.unique(target_info['orientations']))
+target_name = [target_name]
+
+for id in idlist:
+    fig, axs = np.subplots(nrows=n_orientations, ncols=len(bands), figsize=(20, 20))
+    for o in np.arange(1, n_orientations + 1):
+        for b, band in enumerate(bands):
+            idx = np.where(band_info[band]['ids'] == id)[0]
+            files = band_info[band]['files'][idx]
+            orientations = band_info[band]['orientations'][idx]
+            oidx = np.where(orientations == o)[0]
+            file = files[oidx]
+            data = load_fits(file)
+            im = axs[o, b].imshow(np.log10(data + np.min(data) + 1e-10), origin='lower', cmap='magma')
+            axs[o, b].axis('off')
+            axs[o, b].set_title('{} {}'.format(band, orientations[o]))
+            fig.colorbar(im, ax=axs[o, b])
+    plt.tight_layout()
+    plt.title('ID: {}'.format(id))
+    plt.savefig(os.path.join(output_path, 'Image_{}.png'.format(id)))
+
+
+
+"""
 for i_batch, batch in tqdm(enumerate(train_dataloader), total=len(train_dataloader), desc='Plotting Samples'):
     inputs = torch.permute(batch['input'][tio.DATA], (0, 4, 2, 3, 1)).numpy()
     targets = torch.permute(batch['target'][tio.DATA], (0, 4, 2, 3, 1)).numpy()
@@ -231,3 +345,4 @@ for i_batch, batch in tqdm(enumerate(train_dataloader), total=len(train_dataload
         plt.tight_layout()
         plt.savefig(os.path.join(output_path, 'Input_Bands_{}_{}_{}.png'.format(i, ids[i], orientations[i])))
         plt.close()
+"""
