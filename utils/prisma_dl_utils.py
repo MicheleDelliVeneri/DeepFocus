b7cfa42c6ad0ce9ee1b8c6efd0755c7534948a04
Michele Delli Veneri <micheledelliveneri@gmail.com>
2023-04-12 16:49:55 +0200

    Updated Andrea's dataloader

--- utils/prisma_dl_utils.py
+++ utils/prisma_dl_utils.py
@@ -1,5 +1,4 @@
-from astropy.io.fits import getheader
-from astropy.io import fits
+# %%
 import numpy as np
 import torch
 from tqdm import tqdm
@@ -9,56 +8,78 @@ from scipy.ndimage import rotate
 import rasterio
 import patchify
 from torchvision import transforms
+import matplotlib.pyplot as plt
+import torchio as tio
+import os
+from torch.utils.data import DataLoader
 
-
-###### the PRISMA preprocessing datacube 
-
-def prisma_preprocess(path: str):
+# %%
+def prisma_preprocess(images_path: str, training_path: str, file_name : str, train_num : int):
+  """
+  Loads the PRISMA datacubes and preprocess them to be used in the training. Loads from images_path as a numpy 
+  array, rotates each slice, cuts the black stripes, cuts the absorption bands due to the atmosphere and saves.
+  """
   #convert tif file in numpy array
-  numpy_array = rasterio.open(path).read()
+  numpy_array = rasterio.open(images_path).read() # type: ignore
   # rotate the image
   lst=[]
   for i in range (numpy_array.shape[0]):
     lst.append(rotate(numpy_array[i,:,:], angle=14.1, reshape=False, order=0))
   numpy_array = np.array(lst)
+  del lst
   #cut the black stripes
   numpy_array = numpy_array [:,150:1100, 150:1100]
   # if the array is not a mask, cut the absorption bands due to the atmosphere
   if (numpy_array.shape[0] > 1):
     numpy_array = np.concatenate((numpy_array[0:95], numpy_array[109:143], numpy_array[161:]), axis=0)
-  return numpy_array
- 
-def create_dataset(numpy_datacube, patch_bands, patch_height, patch_width, patch_overlap):
-  data_patch = patchify.patchify(numpy_datacube, (patch_bands,patch_height,patch_width), patch_overlap)
-  dataset = data_patch.reshape(data_patch.shape[0] * data_patch.shape[1]* data_patch.shape[2] , data_patch.shape[3], data_patch.shape[4], data_patch.shape[5])
-  return dataset   
+    if (train_num > 1):
+      for i in range (0, 360 + 360//train_num, 360//train_num):
+        numpy_array_rot = np.array([rotate(numpy_array[j,:,:], i, mode='reflect', reshape=False, order=0) for j in range (numpy_array.shape[0])])
+        numpy_array_rot = np.transpose(numpy_array_rot, (2,1,0))
+        numpy_array_rot = np.expand_dims(numpy_array_rot, axis=0)
+        torch.save(torch.from_numpy(numpy_array_rot), training_path + file_name + str(i) + '.pt')
+        del numpy_array_rot
+    else:    
+      numpy_array = np.transpose(numpy_array, (2,1,0))
+      numpy_array = np.expand_dims(numpy_array, axis=0)
+      torch.save(torch.from_numpy(numpy_array), training_path + file_name + '.pt')
+    del numpy_array
+  return(print("the PRISMA datacubes are preporcessed and saved"))
 
-class PrismaDataset(Dataset):
-  def __init__(self, datacubes, masks, transforms = None):
-    # pass the datacube and mask previously built
-    self.datacubes = datacubes
-    self.masks = masks
-    self.transforms = transforms
-  
-  def __len__(self):
-    # return the number of total samples contained in the dataset
-    return len(self.datacubes)
 
-  def __getitem__(self, idx):
-    datacube = self.datacubes[idx]
-    mask = self.masks[idx]
+# %%
+def torch_reader(path):
+    data = torch.load(path)
+    affine = None
+    return data, affine
+# %%
+subject_lst = []
+for file in os.listdir(training_path):
+    file_path = os.path.join(training_path, file)
+    subject = tio.Subject(
+        image = tio.ScalarImage(file_path, reader=torch_reader)
+    )
+    subject_lst.append(subject)
 
-    if self.transforms is not None:
-      datacube = self.transforms(datacube.reshape(datacube.shape[1], datacube.shape[2], datacube.shape[0]))
-      mask =  self.transforms(mask.reshape(mask.shape[1], mask.shape[2], mask.shape[0]))
-    else:
-      datacube = torch.from_numpy(datacube)
-      mask = torch.from_numpy(mask)
 
-    return (datacube, mask)
 
-transforms  = transforms.Compose([
-                transforms.ToTensor(),
-                transforms.RandomRotation(90),
-                transforms.RandomVerticalFlip(p=1)
-                ])	
+# %%
+sardinia_dataset = tio.SubjectsDataset(subject_lst)
+# %%
+patch_size = 96
+queue_length = 300
+samples_per_volume = 10
+sampler = tio.data.UniformSampler(patch_size)
+patches_queue = tio.Queue(
+    sardinia_dataset,
+    queue_length,
+    samples_per_volume,
+    sampler,
+    num_workers=4,
+)
+# %%
+patches_loader = DataLoader(
+    patches_queue,
+    batch_size=16,
+    num_workers=0,  # this must be 0
+)
