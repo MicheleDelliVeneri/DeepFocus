2002ed3fd63aee5c6eb75b3789825676687f3f42
Michele Delli Veneri <micheledelliveneri@gmail.com>
2023-04-12 16:50:05 +0200

    ok

--- utils/dl_utils.py
+++ utils/dl_utils.py
@@ -645,11 +645,65 @@ def get_mosaic_inference_dataloader(root_dir, model_input_dim, cnn_padding, batc
         samples.append(sample)
     
         
+#### --------- Andrea Dataloader ------------ ####
+from scipy.ndimage import rotate
 
-        
+def preprocess(images_path: str, training_path: str, file_name : str, train_num : int):
+  """
+  Loads the PRISMA datacubes and preprocess them to be used in the training. Loads from images_path as a numpy 
+  array, rotates each slice, cuts the black stripes, cuts the absorption bands due to the atmosphere and saves.
+  """
+  #convert tif file in numpy array
+  #numpy_array = rasterio.open(images_path).read() # type: ignore
+  numpy_array = load_fits(images_path)
+  # rotate the image
+  lst=[]
+  for i in range (numpy_array.shape[0]):
+    lst.append(rotate(numpy_array[i,:,:], angle=14.1, reshape=False, order=0))
+  numpy_array = np.array(lst)
+  del lst
+  #cut the black stripes
+  # if the array is not a mask, cut the absorption bands due to the atmosphere
+  if (numpy_array.shape[0] > 1):
+    if (train_num > 1):
+      for i in range (0, 360 + 360//train_num, 360//train_num):
+        numpy_array_rot = np.array([rotate(numpy_array[j,:,:], i, mode='reflect', reshape=False, order=0) for j in range (numpy_array.shape[0])])
+        numpy_array_rot = np.transpose(numpy_array_rot, (2,1,0))
+        numpy_array_rot = np.expand_dims(numpy_array_rot, axis=0)
+        torch.save(torch.from_numpy(numpy_array_rot), training_path + file_name + str(i) + '.pt')
+        del numpy_array_rot
+    else:    
+      numpy_array = np.transpose(numpy_array, (2,1,0))
+      numpy_array = np.expand_dims(numpy_array, axis=0)
+      torch.save(torch.from_numpy(numpy_array), training_path + file_name + '.pt')
+    del numpy_array
+  return(print("the datacubes are preporcessed and saved"))
 
+def torch_reader(path):
+    data = torch.load(path)
+    affine = None
+    return data, affine
 
+def get_subject_list(training_path: str):
+    subject_lst = []
+    for file in os.listdir(training_path):
+        file_path = os.path.join(training_path, file)
+        subject = tio.Subject(
+            image = tio.ScalarImage(file_path, reader=torch_reader)
+        )
+        subject_lst.append(subject)
+    return subject_lst
 
+def get_dataloader(training_path: str, batch_size: int, patch_size: tuple, queque_lenght: int, 
+                    num_workers: 0):
+    subject_lst = get_subject_list(training_path)
+    sampler = tio.data.UniformSampler(patch_size)
+    #transform = tio.Compose([
+    #    tio.RandomFlip(),
+    #    tio.RandomAffine(),
+    patches_queue = tio.Queue(subject_lst, sampler, queque_lenght, num_workers)
+    dataloader = DataLoader(patches_queue, batch_size, num_workers)
+    return dataloader
 
 
 
